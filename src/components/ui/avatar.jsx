1f0f5b60ed2b4e020b46b6d813937d79d85019b7
celebrityscoop868 <celebrityscoop868@gmail.com>
2026-01-30 14:05:15 -0500

    Create avatar.jsx

---
+++ src/components/ui/avatar.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import { cn } from "../../lib/utils";
+
+const Avatar = React.forwardRef(function Avatar({ className, ...props }, ref) {
+  return (
+    <AvatarPrimitive.Root
+      ref={ref}
+      className={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", className)}
+      {...props}
+    />
+  );
+});
+Avatar.displayName = "Avatar";
+
+const AvatarImage = React.forwardRef(function AvatarImage({ className, ...props }, ref) {
+  return (
+    <AvatarPrimitive.Image
+      ref={ref}
+      className={cn("aspect-square h-full w-full object-cover", className)}
+      {...props}
+    />
+  );
+});
+AvatarImage.displayName = "AvatarImage";
+
+const AvatarFallback = React.forwardRef(function AvatarFallback({ className, ...props }, ref) {
+  return (
+    <AvatarPrimitive.Fallback
+      ref={ref}
+      className={cn("flex h-full w-full items-center justify-center rounded-full bg-slate-100 text-slate-600", className)}
+      {...props}
+    />
+  );
+});
+AvatarFallback.displayName = "AvatarFallback";
+
+export { Avatar, AvatarImage, AvatarFallback };
